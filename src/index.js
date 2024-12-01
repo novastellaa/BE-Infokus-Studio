e7e8f4d010587b01b1c05d587a9c8f3ba5e8b90f
novastellaa <novastella01@gmail.com>
2024-12-01 11:08:28 +0700

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { app } from "./application/app.js";
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+    res.send("Backend is running!");
+});
+
 app.listen(PORT, () => {
-  console.log("Server is running on port 3000");
+    console.log("Server is running on port 3000");
 });
